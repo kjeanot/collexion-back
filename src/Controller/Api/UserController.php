f43404b2d43de6a3790fa4ffd4f47bd13cc110b2
AdrienForbice93 <didead93@gmail.com>
2024-02-09 14:24:43 +0100

    api - UserController road UPDATE and DELETE ok

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Entity\User;
+use App\Entity\Comment;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -83,7 +84,7 @@ class UserController extends AbstractController
      * @return Response
      */
     #[Route('/user/create', name: 'app_user_create',methods: ['POST'])]
-    public function create(Request $request, EntityManagerInterface $manager): Response
+    public function create(EntityManagerInterface $manager): Response
     {
         $user = new User();
         // set
@@ -99,5 +100,62 @@ class UserController extends AbstractController
         $manager->flush();
 
         return $this->json([201, ['message' => 'create successful']]);
+    }
+
+        /**
+    * update one user
+    *
+    * @param UserRepository $userRepository
+    * @return Response
+    */
+    #[Route('/user/update/{id}', name: 'api_user_update',methods: ['PUT'])]
+    public function update(User $user = null, EntityManagerInterface $entityManager): Response
+    {
+        // check if $user doesn't exist
+        if (!$user) {
+            return $this->json(
+                "Error : User inexistant",
+                // status code
+                404
+            );
+        }
+
+        $user->setEmail('testzrtyhzrthszrthsrgfhdfg@test.com');
+        $user->setNickname('test');
+        $user->setDescription('Cheesecake macaroni cheese melted cheese. Cheese strings macaroni cheese cheesecake say cheese manchego airedale squirty cheese parmesan. Cheese and wine goat roquefort squirty cheese melted cheese who moved my cheese emmental mascarpone. Feta cheese strings danish fontina.
+        Cheese and biscuits edam cauliflower cheese. Chalk and cheese the big cheese airedale monterey jack cottage cheese fromage frais cow say cheese. Halloumi manchego boursin red leicester say cheese roquefort dolcelatte parmesan. Paneer cheese triangles fondue edam lancashire.');
+        $user->setPicture('https://img.colleconline.com/imgdescription/06af8fba638441b0921770665abc0915/ae0fa799-3150-45ed-9f57-f3831d30e6ee.jpg');
+        $user->setPassword(password_hash('user', PASSWORD_BCRYPT));
+        $user->setRoles(['ROLE_USER']);
+
+        $entityManager->flush();
+ 
+        return $this->json(['message' => 'updated successful', 200]);
+ 
+    }
+
+    /**
+    * delete one user
+    * 
+    * @param UserRepository $userRepository
+    * @return Response
+    */
+    #[Route('/user/delete/{id}', name: 'api_my_user_delete', methods: ['DELETE'])]
+    public function delete(User $user = null , EntityManagerInterface $entityManager): Response
+    {
+         // check if $user doesn't exist
+        if (!$user) {
+            return $this->json(
+                ['message' => 'User inexistant'],
+                404,
+                );
+        }
+        // delete 
+        $entityManager->remove($user);
+        // record in database
+        $entityManager->flush();
+
+        return $this->json(['message' => 'delete successful', 200]);
+       
     }
 }
