be181888d38b608e96b3ff6f6c7c6c167ad95ad4
AdrienForbice93 <didead93@gmail.com>
2024-02-09 15:39:09 +0100

    crud - CommentController ok

---
+++ src/Form/CommentType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use App\Entity\MyObject;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+            ->add('myObject', EntityType::class, [
+                'class' => MyObject::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
