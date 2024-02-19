107a9ca134db4253e0e899825276c506555cc560
AdrienForbice93 <didead93@gmail.com>
2024-02-19 10:09:50 +0100

    Api CatégoryControler updated

--- src/Controller/Api/CategoryController.php
+++ src/Controller/Api/CategoryController.php
@@ -18,7 +18,7 @@ class CategoryController extends AbstractController
      * @param CategoryRepository $CategoryRepository
      * @return Response
      */
-    #[Route('/categories', name: 'api_category_list')]
+    #[Route('/categories', name: 'api_categories_list')]
     public function list(CategoryRepository $categoryRepository): Response
     {   
         $categories = $categoryRepository->findAll();
@@ -37,6 +37,85 @@ class CategoryController extends AbstractController
             ['groups' => 'get_categories']
         );
     }
+
+            /**
+     * list all Categories
+     *
+     * @param CategoryRepository $CategoryRepository
+     * @return Response
+     */
+    #[Route('/categories/parents', name: 'api_categories_parents')]
+    public function listParents(CategoryRepository $categoryRepository): Response
+    {   
+        $categoriesParents = $categoryRepository->findAllCategoriesParent();
+
+        if (!$categoriesParents) { 
+            return $this->json(
+                "Error : Categories inexistantes",
+                404
+            );
+        }
+
+        return $this->json(
+            $categoriesParents,
+            200,
+            ['Access-Control-Allow-Origin' => '*'],
+            ['groups' => 'get_categories']
+        );
+    }
+
+                /**
+     * list all Categories
+     *
+     * @param CategoryRepository $CategoryRepository
+     * @return Response
+     */
+    #[Route('/categories/childs', name: 'api_categories_childs')]
+    public function listChilds(CategoryRepository $categoryRepository): Response
+    {   
+        $categoriesChilds = $categoryRepository->findAllCategoriesChild();
+
+        if (!$categoriesChilds) { 
+            return $this->json(
+                "Error : Categories inexistantes",
+                404
+            );
+        }
+
+        return $this->json(
+            $categoriesChilds,
+            200,
+            ['Access-Control-Allow-Origin' => '*'],
+            ['groups' => 'get_categories']
+        );
+    }
+
+               /**
+     * list one category by its id
+     *
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    #[Route('/category/parents/childs/{id}', name: 'api_categories_parents_childs',methods: ['GET'])]
+    public function showChildsById(Category $category, CategoryRepository $categoryRepository): Response
+    {
+
+        $categoriesChilds = $categoryRepository->findAllCategoriesChildById($category->getId());
+
+        if (!$categoriesChilds) {
+            return $this->json(
+                "Error : Categorie parent inexistant",
+                404
+            );
+        }
+
+        return $this->json(
+            $categoriesChilds,
+            200,
+            ['Access-Control-Allow-Origin' => '*'],
+            ['groups' => 'get_categories']
+            );
+    } 
 
            /**
      * list one category by its id
